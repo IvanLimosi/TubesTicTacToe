48611dda479910f783bd74458bcb4795849d507b
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2021-05-05 12:48:55 +0700

    X and O

--- server.py
+++ server.py
@@ -5,13 +5,26 @@ surface = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Tic-Tac-Toe')
 
 grid = Grid()
+
 running = True
+player = "X"
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                grid.get_mouse(pos[0]//200, pos[1]//200, player)
+                if grid.switch_player:
+                    if player == "X":
+                        player = "O"
+                    else:
+                        player = "X"
 
+            grid.print_grid()
+            print (player)
 
     surface.fill((219,219,219))
 
